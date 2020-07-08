7ebbfa019f95dbcf5f9b1715c4603f151be36eb9
KatMaldon <36998937+katmaldon@users.noreply.github.com>
2020-07-08 02:34:56 -0400

    controllers

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,38 @@
 class TodosController < ApplicationController
+
+    before_action :find_todo, only: [:show, :destroy]
+
+    def index
+        @todos = Todo.all
+        render json: @todos
+    end
+
+    def show
+        render json: @todo
+    end
+
+    def create
+        @todo = Todo.create(todo_params)
+        render json: @todo
+    end
+
+    def new
+        @todo = Todo.new
+        render json: @todo
+    end
+
+    def destroy
+        @todo.destroy
+        render json: @todos
+    end
+
+    private
+
+    def todo_params
+        params.require(:todo).permit(:date, :item)
+    end
+
+    def find_todo
+        @todo = todo.find(params[:id])
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,47 @@
 class UsersController < ApplicationController
+
+    before_action :find_user, only: [:show, :destroy]
+
+    def index
+        @users = User.all
+        render json: @users
+    end
+
+    def show
+        render json: @user
+    end
+
+    def create
+        @user = User.create(user_params)
+        # if User.valid?
+        #     session[:user_id] = User.id
+        #     redirect_to user_path(user)
+        # else
+        #     flash[:errors] = User.errors.full_messages
+        #     redirect_to new_user_path
+        # end
+        render json: @user
+    end
+
+    def new
+        @user = User.new
+        render json: @user
+    end
+
+    def destroy
+        @user.destroy
+        render json: @users
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :age, :location, :bio, :interests, :image_url)
+    end
+
+    def find_user
+        @user = User.find(params[:id])
+    end
+
+
 end
