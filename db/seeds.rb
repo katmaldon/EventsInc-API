9772a426f22e89e1660a2c23e7ff0e36f4439a88
KatMaldon <36998937+katmaldon@users.noreply.github.com>
2020-07-09 04:36:20 -0400

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,134 +8,117 @@
 
 require 'faker'
 
-#Event.destroy_all
-User.destroy_all
+Event.destroy_all
+Todo.destroy_all
+# User.destroy_all
 
 
 puts "Seeded"
 
-    # events = Event.create([{
-    #     name: "Elements Festival",
-    #     event_type: "Festival",
-    #     image_url: "some image url",
-    #     date: "09/25/2020",
-    #     time: "3pm",
-    #     event_url: "elementsfest.us",
-    #     ticket: false
+events = Event.create([{
+    name: "Elements",
+    event_type: "festival",
+    image_url: "https://i.imgur.com/V3WecIN.png",
+    date: "09/25/2020",
+    time: "3pm",
+    event_url: "elementsfest.us",
+    ticket: false
+    },
+    {
+    name: "Deadmau5",
+    event_type: "concert",
+    image_url: "https://i.imgur.com/qd4yI0L.png",
+    date: "08/10/2020",
+    time: "10pm",
+    event_url: "deadmau5.com",
+    ticket: true
+    },
+    {
+    name: "Swanlake",
+    event_type: "ballet",
+    image_url: "https://i.imgur.com/buDhJtb.png",
+    date: "11/02/2020",
+    time: "7pm",
+    event_url: "metopera.org",
+    ticket: true
+    },
+    {
+    name: "Burning Man",
+    event_type: "not a festival",
+    image_url: "https://i.imgur.com/rf8SE4j.png",
+    date: "08/29/2021",
+    time: "8am",
+    event_url: "burningman.org",
+    ticket: true
+    },
+    {
+    name: "Amalfi Coast Trip",
+    event_type: "family vacation",
+    image_url: "https://i.imgur.com/snVTPnj.png",
+    date: "07/03/2021",
+    time: "4am",
+    event_url: "amalficoast.com",
+    ticket: false
+    },
+    {
+    name: "St.Barths",
+    event_type: "not a family vacation",
+    image_url: "https://i.imgur.com/FLJRgOV.png",
+    date: "some day",
+    time: "early morning",
+    event_url: "st-barths.com",
+    ticket: false
+    }
+])
+
+todos = Todo.create([{
+    date: "08/04/2020",
+    item: "pre-order tents and bedding, rent car for Elements"
+    },
+    {
+    date: "09/10/2020",
+    item: "order food and drinks for Elements"
+    },
+    {
+    date: "10/20/2020",
+    item: "reserve restaurant after Swanlake"
+    }
+])
+
+
+
+    # 3.times do
+    #     User.create(
+    #         name: Faker::Name.unique.name,
+    #         age: Faker::Number.number(digits: 2),
+    #         location: Faker::Address.city,
+    #         bio: Faker::TvShows::SiliconValley.unique.quote,
+    #         interests: Faker::Book.unique.genre,
+    #         image_url: "https://www.getaway.co.za/wp-content/uploads/2020/03/Baby-sloth.png"
+    #     )
+    # end
+
+
+
+
+    # user_events = UserEvent.create([{
+    #     user_id: 1,
+    #     event_id: 1
     #     },
     #     {
-    #     name: "Deadmau5",
-    #     event_type: "Concert",
-    #     image_url: "some image url",
-    #     date: "08/10/2020",
-    #     time: "10pm",
-    #     event_url: "deadmau5.com",
-    #     ticket: true
+    #     user_id: 1,
+    #     event_id: 3
     #     },
     #     {
-    #     name: "Swanlake",
-    #     event_type: "Ballet",
-    #     image_url: "some image url",
-    #     date: "11/02/2020",
-    #     time: "7pm",
-    #     event_url: "metopera.org",
-    #     ticket: true
+    #     user_id: 2,
+    #     event_id: 3
     #     },
     #     {
-    #     name: "Burning Man",
-    #     event_type: "Not a festival",
-    #     image_url: "some image url",
-    #     date: "08/29/2021",
-    #     time: "8am",
-    #     event_url: "burningman.org",
-    #     ticket: true
+    #     user_id: 3,
+    #     event_id: 6
     #     },
     #     {
-    #     name: "Amalfi Coast Trip",
-    #     event_type: "Family Vacation",
-    #     image_url: "some image url",
-    #     date: "06/29/2021",
-    #     time: "4am",
-    #     event_url: "amalficoast.com",
-    #     ticket: false
-    #     },
-    #     {
-    #     name: "Birthday Surpise for Mehdi",
-    #     event_type: "Party",
-    #     image_url: "some image url",
-    #     date: "07/13/2020",
-    #     time: "3pm",
-    #     event_url: "Color correction glasses and sunset vibes",
-    #     ticket: false
-    #     },
-    #     {
-    #     name: "Mini Burn",
-    #     event_type: "Festival",
-    #     image_url: "some image url",
-    #     date: "08/25/2020",
-    #     time: "3pm",
-    #     event_url: "Burn pit and ATVs",
-    #     ticket: false
+    #     user_id: 3,
+    #     event_id: 5
     #     }
     # ])
-
-    3.times do
-        User.create(
-            name: Faker::Name.unique.name,
-            age: Faker::Number.number(digits: 2),
-            location: Faker::Address.city,
-            bio: Faker::TvShows::SiliconValley.unique.quote,
-            interests: Faker::Book.unique.genre,
-            image_url: "https://www.getaway.co.za/wp-content/uploads/2020/03/Baby-sloth.png"
-        )
-    end
-
-    # tod = Resource.create([{
-
-    #     title: "Open Your Lobby",
-    #     location: "Various locations Brooklyn",
-    #     image: "some image url",
-    #     zip: "all",
-    #     url: "https://www.6sqft.com/here-are-the-nyc-museums-and-theaters-opening-their-lobbies-to-help-protestors/",
-    #     info: "Businesses and institutions all over NYC offer comfort stations, drinks, snacks, and phone charging to protesters"
-    #     },
-    #     {
-    #     title: "Know your rights",
-    #     location: "USA",
-    #     image: "some image url",
-    #     zip: "all",
-    #     url: "https://www.aclu.org/know-your-rights/protesters-rights/",
-    #     info: "The First Amendment protects your right to assemble and express your views through protest."
-    #     },
-    #     {
-    #     title: "What to bring to a protest",
-    #     location: "USA",
-    #     image: "some image url",
-    #     zip: "all",
-    #     url: "https://www.amnestyusa.org/pdfs/SafeyDuringProtest_F.pdf",
-    #     info: "Practical tips for which equipment to bring, which items to avoid, and how to stay safe at protests"
-    #     },
-    # ])
-
-
-    user_events = UserEvent.create([{
-        user_id: 1,
-        event_id: 1
-        },
-        {
-        user_id: 1,
-        event_id: 3
-        },
-        {
-        user_id: 2,
-        event_id: 3
-        },
-        {
-        user_id: 3,
-        event_id: 6
-        },
-        {
-        user_id: 3,
-        event_id: 5
-        }
-    ])
